eeee04b9ec306a2b27317d7fa6d7757e4ae0c533
kalpana Bharath <stonepanakeerthu@gmail.com>
2024-11-29 11:24:57 +0530

    ivf

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import Router components
-import Navbar from "./Components/Navbar/navbar";
+import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
 import Result from "./Components/Result/Result";
 
@@ -9,7 +9,7 @@ const App = () => {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/IVF" element={<Home />} />
         <Route path="/result" element={<Result />} />
       </Routes>
     </Router>
